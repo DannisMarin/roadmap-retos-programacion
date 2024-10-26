39c487639d654b9796f133172d80cc03bd817064
Dannis Marin <elprofesional69@gmail.com>
2024-10-25 22:24:55 -0400

    editandi mi primer archivo de ejercicio

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DannisMarin.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DannisMarin.py
@@ -1,1 +1,32 @@
 #Mi Primer Comentario
+
+"""
+Otro Comentario en varias 
+l
+i
+n
+e
+a
+s
+"""
+
+'''
+Esto también es
+    un comentario
+        en varias líneas con 
+        comillas simple
+'''
+
+mi_variable = "Mi variable"
+mi_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"  # por convención
+
+mi_int = 10
+mi_float = 7.14
+mi_bool = True
+mi_bool = False
+mi_string = "Mi cadena de texto"
+mi_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Python!")
